e071f85f85c32ea4f5da65594e4020a3bb375569
Mã Hoàng Nhật Phi <mahoangnhatphi@gmail.com>
2018-03-20 18:43:30 +0700

    Commit

--- QuanLyKhoa/frmLogin.Designer.cs
+++ QuanLyKhoa/frmLogin.Designer.cs
@@ -43,7 +43,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, -3);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(660, 422);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
